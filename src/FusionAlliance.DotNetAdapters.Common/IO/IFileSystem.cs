96781b0f79b8815aa469ec811b87b1f5e60d28dd
Brian Wortman <brian.wortman@outlook.com>
2014-07-14 10:57:12 -0400

    Added IFileSystem, a facade for IFile, IPath, and IDirectory. Also added resharper shared settings file for solution.

---
+++ src/FusionAlliance.DotNetAdapters.Common/IO/IFileSystem.cs
@@ -0,0 +1,17 @@
+﻿namespace FusionAlliance.DotNetAdapters.Common.IO
+{
+    /// <summary>
+    /// File system facade.
+    /// </summary>
+    public interface IFileSystem : IFile, IPath, IDirectory
+    {
+        string ReadAllText(string path);
+        void WriteAllText(string path, string contents);
+        void AppendAllText(string path, string contents);
+        bool Exists(string path);
+        void Delete(string path);
+        string Combine(string path1, string path2);
+        void CreateDirectory(string path);
+        void DeleteDirectory(string path);
+    }
+}
