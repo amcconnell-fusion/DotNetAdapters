e4a36edeb925a4a63b1cc470d2baff157bf5a940
Jamie Kurtz <jkurtz@fusionalliance.com>
2014-07-15 09:08:23 -0400

    slight clean-up on IFileSystem facade interface

--- src/FusionAlliance.DotNetAdapters.Common/IO/IFileSystem.cs
+++ src/FusionAlliance.DotNetAdapters.Common/IO/IFileSystem.cs
@@ -5,13 +5,5 @@
     /// </summary>
     public interface IFileSystem : IFile, IPath, IDirectory
     {
-        string ReadAllText(string path);
-        void WriteAllText(string path, string contents);
-        void AppendAllText(string path, string contents);
-        bool Exists(string path);
-        void Delete(string path);
-        string Combine(string path1, string path2);
-        void CreateDirectory(string path);
-        void DeleteDirectory(string path);
     }
 }
