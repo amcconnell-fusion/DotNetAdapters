08df87a1fbfba43c9fb18779cc43cc137014b1ae
Jarrett Meyer <jarrettmeyer@gmail.com>
2014-07-25 14:21:44 -0400

    Adds file, directory, and path adapters. Modifies file system to compose these adapters.

---
+++ src/FusionAlliance.DotNetAdapters.Common/IO/PathAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace FusionAlliance.DotNetAdapters.Common.IO
+{
+    public class PathAdapter : IPath
+    {
+        public string Combine(string path1, string path2)
+        {
+            return Path.Combine(path1, path2);
+        }
+    }
+}
