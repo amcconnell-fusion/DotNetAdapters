c24ed20fc815bec80996059564a7ddd67b060941
Brian Wortman <brian.wortman@outlook.com>
2014-07-26 13:36:19 -0400

    ran code cleanup

--- src/FusionAlliance.DotNetAdapters.Common/IO/FileSystemAdapter.cs
+++ src/FusionAlliance.DotNetAdapters.Common/IO/FileSystemAdapter.cs
@@ -1,12 +1,10 @@
-﻿using System.IO;
-
-namespace FusionAlliance.DotNetAdapters.Common.IO
+﻿namespace FusionAlliance.DotNetAdapters.Common.IO
 {
     public class FileSystemAdapter : IFileSystem
     {
-        private readonly IDirectory directory;
-        private readonly IFile file;
-        private readonly IPath path;
+        private readonly IDirectory _directory;
+        private readonly IFile _file;
+        private readonly IPath _path;
 
         public FileSystemAdapter()
             : this(new DirectoryAdapter(), new FileAdapter(), new PathAdapter())
@@ -15,49 +13,49 @@ namespace FusionAlliance.DotNetAdapters.Common.IO
 
         public FileSystemAdapter(IDirectory directory, IFile file, IPath path)
         {
-            this.directory = directory;
-            this.file = file;
-            this.path = path;
+            _directory = directory;
+            _file = file;
+            _path = path;
         }
 
         public string ReadAllText(string path)
         {
-            return file.ReadAllText(path);
+            return _file.ReadAllText(path);
         }
 
         public void WriteAllText(string path, string contents)
         {
-            file.WriteAllText(path, contents);
+            _file.WriteAllText(path, contents);
         }
 
         public void AppendAllText(string path, string contents)
         {
-            file.AppendAllText(path, contents);
+            _file.AppendAllText(path, contents);
         }
 
         public bool Exists(string path)
         {
-            return file.Exists(path);
+            return _file.Exists(path);
         }
 
         public void Delete(string path)
         {
-            file.Delete(path);
+            _file.Delete(path);
         }
 
         public string Combine(string path1, string path2)
         {
-            return path.Combine(path1, path2);
+            return _path.Combine(path1, path2);
         }
 
         public void CreateDirectory(string path)
         {
-            directory.CreateDirectory(path);
+            _directory.CreateDirectory(path);
         }
 
         public void DeleteDirectory(string path)
         {
-            directory.DeleteDirectory(path);
+            _directory.DeleteDirectory(path);
         }
     }
 }
