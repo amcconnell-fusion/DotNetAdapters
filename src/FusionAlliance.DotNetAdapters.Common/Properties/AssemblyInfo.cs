8ab4894474d05ab2df9aeadf51ecbc3349eb1a11
Brian Wortman <brian.wortman@outlook.com>
2014-07-28 18:20:10 -0400

    Bumped version. Switching to SemVer.

--- src/FusionAlliance.DotNetAdapters.Common/Properties/AssemblyInfo.cs
+++ src/FusionAlliance.DotNetAdapters.Common/Properties/AssemblyInfo.cs
@@ -28,12 +28,9 @@ using System.Runtime.InteropServices;
 //
 //      Major Version
 //      Minor Version 
-//      Build Number
-//      Revision
+//      Patch Number
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// See http://docs.nuget.org/docs/Reference/Versioning for details.
 
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
