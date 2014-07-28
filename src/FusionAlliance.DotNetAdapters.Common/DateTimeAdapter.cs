74f6218adfc68aa56ea0931559a5bde2ca357b1f
Brian Wortman <brian.wortman@outlook.com>
2014-07-28 18:09:04 -0400

    added new methods to DateTimeAdapter

--- src/FusionAlliance.DotNetAdapters.Common/DateTimeAdapter.cs
+++ src/FusionAlliance.DotNetAdapters.Common/DateTimeAdapter.cs
@@ -4,6 +4,16 @@ namespace FusionAlliance.DotNetAdapters.Common
 {
     public class DateTimeAdapter : IDateTime
     {
+        public DateTime Now
+        {
+            get { return DateTime.Now; }
+        }
+
+        public DateTime Today
+        {
+            get { return DateTime.Today; }
+        }
+
         public DateTime UtcNow
         {
             get { return DateTime.UtcNow; }
